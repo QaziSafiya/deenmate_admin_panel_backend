a42c04fb6cd6b940e530e3d8966db11eb573d51b
safiya qazi <safiyaqazi517@gmail.com>
2025-08-05 12:19:41 +0530

    new changes in backend

--- index.ts
+++ index.ts
@@ -42,4 +42,6 @@ app.get("/", async (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 
-//  index file 
+//  index file
+//  index file
+// index file
